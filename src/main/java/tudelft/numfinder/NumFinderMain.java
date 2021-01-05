deae2ec5b8547165cefe282b0270af68f1356394
Melvin Jacobson <melvin@soonapp.io>
2021-01-05 17:19:06 +0100

    some manual (main) tests

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -5,11 +5,18 @@ public class NumFinderMain {
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+        //  this works
+        //  nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        // nf.find(new int[] {4, 3, 2, 1});
+
+        // null
+        // nf.find(null);
+
+        // over the edge
+        nf.find(new int[] {Integer.MIN_VALUE - 1, 0, Integer.MAX_VALUE + 1});
+
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
